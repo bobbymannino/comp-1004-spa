7deaa75d9e6e159dadc62b23dd7d9e8564b20daa
Bobby Mannino <manninobobby@icloud.com>
2023-11-21 19:56:35 +0000

    more refactoring

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-/**
- * The current date updated every second
- * */
+/** The current date as normal and updated every second */
 let date = new Date();
+
+/** The current date formatted and updated every second */
+let currentDate = {
+  /** Seconds */
+  ss: date.getSeconds(),
+  /** Minutes */
+  mm: date.getMinutes(),
+  /** Hours */
+  hh: date.getHours(),
+  /** Day of the month */
+  dd: date.getDate(),
+  /** Year */
+  yyyy: date.getFullYear(),
+  /** 3 letter day */
+  "day-short": date.toLocaleDateString("en-US", { weekday: "short" }),
+  /** Full day */
+  day: date.toLocaleDateString("en-US", { weekday: "long" }),
+  /** Month of the year */
+  "month-num": date.getMonth() + 1,
+  /** 3 letter month */
+  "month-short": date.toLocaleDateString("en-US", { month: "short" }),
+  /** Full month */
+  month: date.toLocaleDateString("en-US", { month: "long" }),
+};
 
 /**
  * Updates the date variable and replaces all the elements text with the data-replace-with attribute
  */
 function updateDate() {
   date = new Date();
+
+  currentDate = {
+    /** Seconds */
+    ss: date.getSeconds(),
+    /** Minutes */
+    mm: date.getMinutes(),
+    /** Hours */
+    hh: date.getHours(),
+    /** Day of the month */
+    dd: date.getDate(),
+    /** Year */
+    yyyy: date.getFullYear(),
+    /** 3 letter day */
+    "day-short": date.toLocaleDateString("en-US", { weekday: "short" }),
+    /** Full day */
+    day: date.toLocaleDateString("en-US", { weekday: "long" }),
+    /** Month of the year */
+    "month-num": date.getMonth() + 1,
+    /** 3 letter month */
+    "month-short": date.toLocaleDateString("en-US", { month: "short" }),
+    /** Full month */
+    month: date.toLocaleDateString("en-US", { month: "long" }),
+  };
 
   // Replace all the elements text with the data-replace-with attribute
   [...document.querySelectorAll("[data-replace-with]")].forEach(replaceWith);
@@ -20,60 +65,31 @@ function updateDate() {
 function replaceWith(el) {
   /**
      format can equal the following things;
+     - ss = seconds = 05
+     - mm = minutes = 05
+     - hh = hour = 12
      - dd = day of the month = 05
-     - mm = month of the year = 03
-     - yy = year = 21
      - yyyy = year = 2021
-     - day = The current day - Monday
+     - yy = year = 21
      - day-short - 3 letter day - Mon
-     - month - The current month - November
+     - day = The current day - Monday
+     - month-num = month of the year = 03
      - month-short - 3 letter month - Nov 
+     - month - The current month - November
+     They will be formatted in that order
      */
   const format = el.getAttribute("data-replace-with");
   if (!format) return;
 
-  let newText = "";
+  let currentText = el.textContent;
+  let newText = format;
 
-  switch (format) {
-    case "dd": {
-      newText = date.getDate();
-      break;
-    }
-    case "mm": {
-      newText = date.getMonth() + 1;
-      break;
-    }
-    case "yy": {
-      newText = date.getFullYear().toString().slice(-2);
-      break;
-    }
-    case "yyyy": {
-      newText = date.getFullYear();
-      break;
-    }
-    case "day": {
-      newText = date.toLocaleDateString("en-US", { weekday: "long" });
-      break;
-    }
-    case "day-short": {
-      newText = date.toLocaleDateString("en-US", { weekday: "short" });
-      break;
-    }
-    case "month": {
-      newText = date.toLocaleDateString("en-US", { month: "long" });
-      break;
-    }
-    case "month-short": {
-      newText = date.toLocaleDateString("en-US", { month: "short" });
-      break;
-    }
-    default: {
-      newText = "Invalid format";
-    }
-  }
+  Object.entries(currentDate).forEach(([key, value]) => {
+    newText = newText.replace(new RegExp(key, "g"), value);
+  });
 
   // Only update the text if it has changed
-  if (newText !== el.textContent) el.textContent = newText;
+  if (currentText !== newText) el.textContent = newText;
 }
 
 // What to do on initialization
