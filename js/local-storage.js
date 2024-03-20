fc284f34c32d47e1f61fa9eb056158c33472b8ca
Bobby Joe Mannino <manninobobby@icloud.com>
2024-03-20 09:51:12 +0000

    messing around now

--- js/local-storage.js
+++ js/local-storage.js
@@ -19,3 +19,46 @@ function getCalendarDataFromLocalStorage() {
 function saveCalendarDataToLocalStorage(data) {
     localStorage.setItem("calendarData", JSON.stringify(data));
 }
+
+/**
+ * Creates a local storage store with history that updates everytime is is changed and is recovered on load
+ * @param {string} key The key for the local storage
+ * @param {any} initialValue The initial value if no value is found in local storage 
+ * @returns {typeof initialValue} A value with the same type of initial value
+ */
+function createStore(key, initialValue) {
+    let data = localStorage.getItem(key);
+
+    /** @type {Store} */
+    let store = { [(new Date()).getTime()]: initialValue };
+
+    if (value) {
+        store = JSON.parse(data);
+    } else {
+        localStorage.setItem(key, JSON.stringify(store));
+    }
+
+    return createProxy(store, (obj, prop, value) => {
+        localStorage.setItem(key,)
+    })
+}
+
+/**
+ * Creates a proxy and returns it
+ * @param {object} initialValue The intial value
+ * @param { (obj, prop, value) => {} } onSet A function to do everytime the variable is set
+ * @returns {typeof initialValue}
+ */
+function createProxy(intialValue, onSet) {
+    return new Proxy(initialValue,
+        {
+            set: (obj, prop, val) => {
+                obj[prop] = val;
+
+                onSet(obj, prop, value);
+
+                return true;
+            },
+        }
+    );
+}
